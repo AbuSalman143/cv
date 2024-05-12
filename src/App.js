479fcfd52a9f6022808f0a5fdb3afd724144a7a8
Abu Salman <121804012+AbuSalman143@users.noreply.github.com>
2024-05-12 13:12:09 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from "react";
-import  ReactDOM  from "react-dom";
+// import React from "react";
+// import  ReactDOM  from "react-dom";
 import Header from "./componet/Header";
 import About from "./componet/About";
 import MainSection from "./componet/MainSection";
@@ -8,7 +8,6 @@ import Footer from "./componet/Footer";
 
 
 let p1="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam, natus, rem, quibusdam commodi pariatur nisi ducimus provident excepturi officia reiciendis nesciunt? Dignissimos explicabo consequatur ut delectus aliquid amet voluptas fugit.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam, natus, rem, quibusdam commodi pariatur nisi ducimus provident excepturi officia reiciendis nesciunt? Dignissimos explicabo consequatur ut delectus aliquid amet voluptas fugit."
-
 let p2="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam, natus, rem, quibusdam commodi pariatur nisi ducimus provident excepturi officia reiciendis nesciunt? Dignissimos explicabo consequatur ut delectus aliquid amet voluptas fugit.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam, natus, rem, quibusdam commodi pariatur nisi ducimus provident excepturi officia reiciendis nesciunt? Dignissimos explicabo consequatur ut delectus aliquid amet voluptas fugit.";
 let p3="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam, natus, rem, quibusdam commodi pariatur nisi ducimus provident excepturi officia reiciendis nesciunt? Dignissimos explicabo consequatur ut delectus aliquid amet voluptas fugit.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam, natus, rem, quibusdam commodi pariatur nisi ducimus provident excepturi officia reiciendis nesciunt? Dignissimos explicabo consequatur ut delectus aliquid amet voluptas fugit."
 function App(){
@@ -18,8 +17,8 @@ function App(){
                         <About/>
                         <div className="section">
                         <MainSection h1="Skill" p={p1} id="#skill" />
-                        <MainSection h1="Qalfication" p={p1} id="#qualification"/>
-                        <MainSection h1="Project" p={p1} id="#project"/>
+                        <MainSection h1="Qalfication" p={p2} id="#qualification"/>
+                        <MainSection h1="Project" p={p3} id="#project"/>
                         </div>
                         <Footer/>
                 </div>
